01af1aec226f1e2d59510411b4ea1523c217f6d7
Saket Kumar Sinha <133163978+sinha-19@users.noreply.github.com>
2025-06-09 02:20:51 +0530

    Create LCAinBST.java

---
+++ Day 5/LCAinBST.java
@@ -0,0 +1,14 @@
+/*
+ * Problem: Lowest Common Ancestor in BST.
+ * Time: O(H), Space: O(H)
+ */
+class LCAinBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) root = root.left;
+            else if (p.val > root.val && q.val > root.val) root = root.right;
+            else return root;
+        }
+        return null;
+    }
+}
